2bce3aafb456d6eddbe48c7c18db135abf8e2828
BalancedmonkeY <clareece_@hotmail.com>
2017-07-07 17:48:21 +0100

    Added false-positive rate statistics. Made it so the buttons were normal, not circles. Reorganised the plot options drop down. Made the plot option reactive with a vector which is printed at the bottom

--- MAofDTA.R
+++ MAofDTA.R
@@ -12,22 +12,24 @@ ui <- fluidPage(
   fluidRow(titlePanel("Msc Project!")), #title
   
   fluidRow(column(3, wellPanel( fluidRow(p("Plot options")),
-                                fluidRow(dropdownButton(label = "Choose options", status = "primary", width = 80,
+                                fluidRow(dropdownButton(label = "Choose options", circle=F, status = "primary", width = 80,
                                                         checkboxGroupInput(inputId = "plotcheck", label = "Choose", 
-                                                        choices = list("Point estimate"=1, "Data points"=2, "Confidence region"=3, "Predictive region"=4, "Individual confidence regions"=5),
+                                                        choices = list("Point estimate"=1, "Confidence region"=2, "Predictive region"=3, "Extrapolate"=4, "Data points"=5, "Individual confidence regions"=6),
                                                         selected=list(1,2,3)))) #selected shows the default ticked boxes
                   )), #gives options to alter the aesthetics of the plot
            
            column(6, plotOutput(outputId="SROC"))), #graph
   
   fluidRow(column(6, offset=3, wellPanel( fluidRow(column(6, p("Bivariate Meta-Analysis Statistics")), 
-                                                   column(6, dropdownButton(label="Choose statistics", status="primary", width=80,
+                                                   column(6, dropdownButton(label="Choose statistics", circle=F, status="primary", width=80,
                                                                             checkboxGroupInput(inputId="statscheck", label="Choose", #drop down menu for the statistics
-                                                                            choices=list("Sensitivity"=1, "Specificity"=2, "AUC"=3), selected=list(1,2))) )),
+                                                                            choices=list("Sensitivity"=1, "Specificity"=2, "AUC"=3, "FPR"=4), selected=list(1,2))) )),
                                           conditionalPanel( condition="output.senscheck", textOutput("sens")), #conditional statement is a reactive R equality (when true the panel will run)
                                           conditionalPanel( condition="output.speccheck", textOutput("spec")),
-                                          conditionalPanel( condition="output.AUCcheck", textOutput("AUC")) ))) #statistics conditional on being ticked
-
+                                          conditionalPanel( condition="output.AUCcheck", textOutput("AUC")),
+                                          conditionalPanel( condition="output.FPRcheck", textOutput("FPR")) ))), #statistics conditional on being ticked
+ 
+  fluidRow( textOutput(outputId="logical")) #print results from logical vector
 )
 
 #-------------------------------------------------------------------------#
@@ -49,20 +51,34 @@ legend("bottomleft", c("SROC", "conf. region"), lwd = c(2,1))
 } )
   
   # Will need a basic ROC curve without anything else, to then be able to add the other parts on top
-  #going to need a "middle man (plot) similar to the middle data example#
+  #going to need a "middle man (plot) similar to the middle data example -> vector of true/false#
+  #toggle pieces: data, summary estimate, conf region, pred region, extrapolate
+  #Plot options vector printout
+  plotticks<-logical(length=6) #default of six Falses
+    output$logical <- renderPrint({ if ('1' %in% input$plotcheck) {plotticks[1] <- T} #change plotticks to T if ticked
+                                    if ('2' %in% input$plotcheck) {plotticks[2] <- T}
+                                    if ('3' %in% input$plotcheck) {plotticks[3] <- T}
+                                    if ('4' %in% input$plotcheck) {plotticks[4] <- T}
+                                    if ('5' %in% input$plotcheck) {plotticks[5] <- T}
+                                    if ('6' %in% input$plotcheck) {plotticks[6] <- T}
+                                  print(plotticks)}) #make interactive text
+  
  
   #Add some stats
   stats<-summary(fit.reitsma) #output statistics from the fit
   output$sens <- renderText(print(sprintf("Sensitivity: %4.3f", stats$coefficients[3,1]), quote=F)) #prints sensitivity
   output$spec <- renderText(print(sprintf("Specificity: %4.3f", 1-stats$coefficients[4,1]), quote=F)) #prints specificity
   output$AUC <- renderText(print(sprintf("AUC: %4.3f", stats$AUC[1]), quote=F)) #prints AUC
+  output$FPR <- renderText(print(sprintf("False-positive rate: %4.3f", stats$coefficients[4,1]), quote=F)) #prints FPrate
   #Making the print statistics interactive with the pulldown menu:
   output$senscheck <- reactive({'1' %in% input$statscheck}) #reactive boolean to see whether '1' (sensitivity has been checked) is in the checklist
   output$speccheck <- reactive({'2' %in% input$statscheck}) #needs to be reactive as can't do the correct condition in JavaScript
   output$AUCcheck <- reactive({'3' %in% input$statscheck})
+  output$FPRcheck <- reactive({'4' %in% input$statscheck})
 outputOptions(output, "senscheck", suspendWhenHidden = FALSE) #option needed for conditional to function properly
 outputOptions(output, "speccheck", suspendWhenHidden = FALSE)
 outputOptions(output, "AUCcheck", suspendWhenHidden = FALSE)
+outputOptions(output, "FPRcheck", suspendWhenHidden = FALSE)
 }
 
 #-----------------------------------------------------------#
