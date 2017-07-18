5a0144ee1e74ee9b824ce519c192444b04cf7c76
BalancedmonkeY <clareece_@hotmail.com>
2017-07-18 16:42:45 +0100

    Added DOR and Likelihood ratios to statistics and rejigged statistics table a little. Also renamed statistics object

--- MAofDTA.R
+++ MAofDTA.R
@@ -16,11 +16,14 @@ ui <- fluidPage(
                              fluidRow(column(6, checkboxInput("intervals", label="Confidence intervals")),#checkbox for option to show confidence intervals
                                       column(6, dropdownButton(label="Choose statistics", circle=F, status="primary", width=80, size="sm",
                                                                checkboxGroupInput(inputId="statscheck", label="Choose", #drop down menu for the statistics
-                                                                                 choices=list("Sensitivity"=1, "Specificity"=2, "AUC"=3, "FPR"=4), selected=list(1,2))) )), 
-                             conditionalPanel( condition="output.senscheck", fluidRow(column(5, p("Sensitivity")), column(2, textOutput("sens")), column(5, conditionalPanel(condition="input.intervals", textOutput("sensCI"))))), #conditional statement is a reactive R equality (when true the panel will run)
-                             conditionalPanel( condition="output.speccheck", fluidRow(column(5, p("Specificity")), column(2, textOutput("spec")), column(5, conditionalPanel(condition="input.intervals", textOutput("specCI"))))), #conditional confidence intervals
-                             conditionalPanel( condition="output.AUCcheck", fluidRow(column(5, p("AUC")), column(2, textOutput("AUC")))),
-                             conditionalPanel( condition="output.FPRcheck", fluidRow(column(5, p("False-positivie rate")), column(2, textOutput("FPR")), column(5, conditionalPanel(condition="input.intervals", textOutput("FPRCI"))))))),
+                                                                                 choices=list("Sensitivity"=1, "Specificity"=2, "AUC"=3, "FPR"=4, "DOR"=5, "Likelihood Ratios"=6), selected=list(1,2))) )), 
+                             conditionalPanel( condition="output.senscheck", fluidRow(column(6, p("Sensitivity")), column(2, textOutput("sens")), column(4, conditionalPanel(condition="input.intervals", textOutput("sensCI"))))), #conditional statement is a reactive R equality (when true the panel will run)
+                             conditionalPanel( condition="output.speccheck", fluidRow(column(6, p("Specificity")), column(2, textOutput("spec")), column(4, conditionalPanel(condition="input.intervals", textOutput("specCI"))))), #conditional confidence intervals
+                             conditionalPanel( condition="output.AUCcheck", fluidRow(column(6, p("AUC")), column(2, textOutput("AUC")))),
+                             conditionalPanel( condition="output.FPRcheck", fluidRow(column(6, p("False-positivie rate")), column(2, textOutput("FPR")), column(4, conditionalPanel(condition="input.intervals", textOutput("FPRCI"))))),
+                             conditionalPanel( condition="output.DORcheck", fluidRow(column(6, p("Diagnostic Odds Ratio")), column(2, textOutput("DOR")))),
+                             conditionalPanel( condition="output.LRcheck", fluidRow(column(6, p("Positive Likelihood Ratio")), column(2, textOutput("LRp"))),
+                                                                           fluidRow(column(6, p("Negative Likelihood Ratio")), column(2, textOutput("LRn")))))),
            column(5, plotOutput(outputId="SROC")), #graph
            column(3, wellPanel( fluidRow(p("Plot options")), #plot options
                                 fluidRow(dropdownButton(label = "Choose options", circle=F, status = "primary", width = 80,
@@ -40,7 +43,8 @@ server <- function(input,output) {
   
   #Analyis
   fit.reitsma <- reitsma(AuditC) #fits a bivariate model
-  stats<-summary(fit.reitsma) #output statistics from the fit 
+  sum.fit<-summary(fit.reitsma) #output statistics from the fit 
+  pts.fit <- SummaryPts(fit.reitsma) #outputs posLR, negLR, invnegLR and DOR (as samples, have to mean to extract output)
   
   # Basic ROC curve without anything else, add the other parts on top using an interactive vector (object orientation)
   #toggle pieces: data, summary estimate, conf region, pred region, extrapolate
@@ -65,29 +69,36 @@ server <- function(input,output) {
                                   plot(fit.reitsma, main = "SROC curve (bivariate model) for AUDIT-C data",
                                        extrapolate=plotticks[4], plotsumm=plotticks[2], predict=plotticks[3], pch="") #plot where options are dependent on interactive vector plotticks
                                   if (plotticks[5]==T) {points(fpr(AuditC), sens(AuditC), pch=2)} #add data points
-                                  if (plotticks[1]==T) {points(stats$coefficients[4,1], stats$coefficients[3,1])} #adding summary estimate
+                                  if (plotticks[1]==T) {points(sum.fit$coefficients[4,1], sum.fit$coefficients[3,1])} #adding summary estimate
                                   legend("bottomright", leglabticks, pch = legendticks[,1], lty=legendticks[,2], lwd=c(2,NA,1,1,NA))
                                   }) 
   
  
   #Add some stats
-  output$sens <- renderText(print(sprintf("%4.3f", stats$coefficients[3,1]), quote=F)) #prints sensitivity
-  output$spec <- renderText(print(sprintf("%4.3f", 1-stats$coefficients[4,1]))) #prints specificity
-  output$AUC <- renderText(print(sprintf("%4.3f", stats$AUC[1]))) #prints AUC
-  output$FPR <- renderText(print(sprintf("%4.3f", stats$coefficients[4,1]))) #prints FPrate
+  output$sens <- renderText(print(sprintf("%4.3f", sum.fit$coefficients[3,1]), quote=F)) #prints sensitivity
+  output$spec <- renderText(print(sprintf("%4.3f", 1-sum.fit$coefficients[4,1]))) #prints specificity
+  output$AUC <- renderText(print(sprintf("%4.3f", sum.fit$AUC[1]))) #prints AUC
+  output$FPR <- renderText(print(sprintf("%4.3f", sum.fit$coefficients[4,1]))) #prints FPrate
+  output$DOR <- renderText(print(sprintf("%6.3f", mean(pts.fit$DOR)))) #prints summary DOR
+  output$LRp <- renderText(print(sprintf("%6.3f", mean(pts.fit$posLR)))) #LR+
+  output$LRn <- renderText(print(sprintf("%6.3f", mean(pts.fit$negLR)))) #LR-
   #Making the print statistics interactive with the pulldown menu:
   output$senscheck <- reactive({'1' %in% input$statscheck}) #reactive boolean to see whether '1' (sensitivity has been checked) is in the checklist
   output$speccheck <- reactive({'2' %in% input$statscheck}) #needs to be reactive as can't do the correct condition in JavaScript
   output$AUCcheck <- reactive({'3' %in% input$statscheck})
   output$FPRcheck <- reactive({'4' %in% input$statscheck})
+  output$DORcheck <- reactive({'5' %in% input$statscheck})
+  output$LRcheck <- reactive({'6' %in% input$statscheck})
 outputOptions(output, "senscheck", suspendWhenHidden = FALSE) #option needed for conditional to function properly
 outputOptions(output, "speccheck", suspendWhenHidden = FALSE)
 outputOptions(output, "AUCcheck", suspendWhenHidden = FALSE)
 outputOptions(output, "FPRcheck", suspendWhenHidden = FALSE)
+outputOptions(output, "DORcheck", suspendWhenHidden = FALSE)
+outputOptions(output, "LRcheck", suspendWhenHidden = FALSE)
   #Adding confidence intervals
-  output$sensCI <- renderText(print(sprintf("(%4.3f, %4.3f)", stats$coefficients[3,5], stats$coefficients[3,6]), quote=F))
-  output$specCI <- renderText(print(sprintf("(%4.3f, %4.3f)", 1-stats$coefficients[4,6], 1-stats$coefficients[4,5]), quote=F))
-  output$FPRCI <- renderText(print(sprintf("(%4.3f, %4.3f)", stats$coefficients[4,5], stats$coefficients[4,6]), quote=F))
+  output$sensCI <- renderText(print(sprintf("(%4.3f, %4.3f)", sum.fit$coefficients[3,5], sum.fit$coefficients[3,6]), quote=F))
+  output$specCI <- renderText(print(sprintf("(%4.3f, %4.3f)", 1-sum.fit$coefficients[4,6], 1-sum.fit$coefficients[4,5]), quote=F))
+  output$FPRCI <- renderText(print(sprintf("(%4.3f, %4.3f)", sum.fit$coefficients[4,5], sum.fit$coefficients[4,6]), quote=F))
 }
 
 #-----------------------------------------------------------#
