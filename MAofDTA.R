89edb2227350ede48057990e4407c6a6399aaa12
BalancedmonkeY <clareece_@hotmail.com>
2017-07-20 12:42:29 +0100

    Working higgeldy piggeldy new plot options

--- MAofDTA.R
+++ MAofDTA.R
@@ -13,9 +13,10 @@ library(DT)
 ui <- fluidPage(
   titlePanel("Msc Project!"), #title
   
-    column(5, wellPanel(fluidRow(p("Plot options")), #plot options
-                 fluidRow(checkboxGroupInput(inputId = "plotcheck", label = "Choose", 
-                                            choices = list("Point estimate"=1, "Confidence region"=2, "Predictive region"=3, "Extrapolate"=4, "Data points"=5),
+    column(5, wellPanel(fluidRow(h4("Plot options")), #plot options
+                  fluidRow(checkboxInput(inputId="dataptscheck", label="Data points")),
+                 fluidRow(checkboxGroupInput(inputId = "bivcheck", label = "Bivariate model options", 
+                                            choices = list("Point estimate"=1, "Confidence region"=2, "Predictive region"=3, "Extrapolate"=4, "Data points"=5, "HSROC curve"=6),
                                             selected=list(1,2,3)))),
                  br(),
                  wellPanel(fluidRow(p("Bivariate Meta-Analysis Statistics")), 
@@ -58,26 +59,27 @@ server <- function(input,output) {
   
   # Basic ROC curve without anything else, add the other parts on top using an interactive vector (object orientation)
   #toggle pieces: data, summary estimate, conf region, pred region, extrapolate
-  plotticks<-logical(length=5) #default of six Falses
+  plotticks<-logical(length=6) #default of six Falses
   leglabticks <- matrix(nrow=5, ncol=1)
   legendticks <- matrix(nrow=5, ncol=2) #empty matrix for legend arguments
-  leglabticks[1] <- "SROC curve"
-  legendticks[1,2] <- 1 #legend options for SROC curve
-    output$SROC <- renderPlot({ if ('1' %in% input$plotcheck) {plotticks[1] <- T
+    output$SROC <- renderPlot({ if ('1' %in% input$bivcheck) {plotticks[1] <- T
                                                                 leglabticks[2]<-"Summary estimate"
                                                                 legendticks[2,1]<-1} #change plotticks and legendticks if option 1 selected
-                                if ('2' %in% input$plotcheck) {plotticks[2] <- T
+                                if ('2' %in% input$bivcheck) {plotticks[2] <- T
                                                                 leglabticks[3]<-"Confidence region"
                                                                 legendticks[3,2]<-1} #creates interactive vector
-                                if ('3' %in% input$plotcheck) {plotticks[3] <- T
+                                if ('3' %in% input$bivcheck) {plotticks[3] <- T
                                                                 leglabticks[4]<-"Predictive region"
                                                                 legendticks[4,2]<-3}
-                                if ('4' %in% input$plotcheck) {plotticks[4] <- T} #Extrapolate
-                                if ('5' %in% input$plotcheck) {plotticks[5] <- T
+                                if ('4' %in% input$bivcheck) {plotticks[4] <- T} #Extrapolate
+                                if (input$dataptscheck==T) {plotticks[5] <- T
                                                                 leglabticks[5]<-"Data"
                                                                 legendticks[5,1]<-2}
+                                if ('6' %in% input$bivcheck) {plotticks[6] <- T
+                                                                leglabticks[1]<-"HSROC curve"
+                                                                legendticks[1,2]<-1}
                                   plot(fit.reitsma, main = "SROC curve (bivariate model) for AUDIT-C data",
-                                       HSROC=T, extrapolate=plotticks[4], plotsumm=plotticks[2], predict=plotticks[3], pch="") #plot where options are dependent on interactive vector plotticks
+                                       HSROC=plotticks[6], extrapolate=plotticks[4], plotsumm=plotticks[2], predict=plotticks[3], pch="") #plot where options are dependent on interactive vector plotticks
                                   if (plotticks[5]==T) {points(fpr(AuditC), sens(AuditC), pch=2)} #add data points
                                   if (plotticks[1]==T) {points(sum.fit$coefficients[4,1], sum.fit$coefficients[3,1])} #adding summary estimate
                                   legend("bottomright", leglabticks, pch = legendticks[,1], lty=legendticks[,2], lwd=c(2,NA,1,1,NA))
